7f063335d5483002347ecd5e554d554ccb05e10b
Grvzard <grvzard@outlook.com>
2023-05-13 20:23:31 +0800

    remove deps of pymysqlreplication

--- mysql_replica/targetdb.py
+++ mysql_replica/targetdb.py
@@ -1,11 +1,9 @@
 from abc import ABC, abstractmethod
 
-from pymysqlreplication.row_event import RowsEvent
-
 
 class TargetDb(ABC):
     @abstractmethod
-    async def put(self, e: RowsEvent):
+    async def put(self, event):
         ...
 
     async def open(self):
